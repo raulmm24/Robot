96170aecd4dbea3f1e254dd38ee404a812cca0c0
raulm <raulmartinezmartinez24@gmail.com>
2024-12-02 14:12:56 +0100

    Modificación en ControladorRobot

--- src/main/java/org/iesalandalus/programacion/robot/modelo/ControladorRobot.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/ControladorRobot.java
@@ -1,35 +1,32 @@
 package org.iesalandalus.programacion.robot.modelo;
 
+import java.util.Objects;
+
 public class ControladorRobot {
 
     private Robot robot;
+
     public ControladorRobot(Robot robot){
-        if (robot != null) {
-            try {
-                this.robot =(Robot) robot;
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            throw new NullPointerException("El robot no puede ser nulo.");
-        }
+        Objects.requireNonNull(robot, "El robot no puede ser nulo.");
+        this.robot = new Robot(robot);
     }
     public Robot getRobot() {
         return new Robot(robot);
     }
+
     public void ejecutar(char comando) {
         switch (comando) {
             case 'A':
             case 'a':
-                this.robot.avanzar();
+                robot.avanzar();
             break;
             case 'D':
             case 'd':
-                this.robot.girarALaDerecha();
+                robot.girarALaDerecha();
             break;
             case 'I':
             case 'i':
-                this.robot.girarALaIzquierda();
+                robot.girarALaIzquierda();
             break;
             default: throw new RobotExcepcion("Comando desconocido.");
         }
